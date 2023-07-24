427891cbd3e7a0112d2d7dc17a317a64ee20f746
S-123-satya <satyaprakash5056742@gmail.com>
2023-07-24 13:32:00 +0530

    login page created

--- index.js
+++ index.js
@@ -6,39 +6,59 @@ const User = require('./model/dbmodel');
 const { isBooleanObject } = require('util/types');
 const { json } = require('body-parser');
 const bodyParser = require('body-parser');
-const app=express();
+const app = express();
 
 app.use(cors());
-// app.use(express.json());
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.json());
+// app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
-app.post('/',(req,res)=>{
+app.post('/', (req, res) => {
     console.log(`req.body.name=${req.body.name}`);
     console.log(`req.body.email=${req.body.email}`);
     console.log(`req.body.password=${req.body.password}`);
     console.log(`req.body.User=${req.body}`);
     User.create(req.body)
-    .then(result=>{
-        console.log(result);
-        res.json(result);
-        // res.redirect('/');
+        .then(result => {
+            console.log(result);
+            res.json(result);
+            // res.redirect('/');
+        })
+        .catch(err => {
+            console.log(err)
+            res.send(err);
+        });
+})
+app.post('/login', (req, res) => {
+    console.log(`req.body.email=${req.body.email}`);
+    console.log(`req.body.password=${req.body.password}`);
+    console.log(`req.body.User=${req.body}`);
+    User.findAll({
+        where: {
+            email: req.body.email
+        }
     })
-    .catch(err=>{
-        console.log(err)
-        res.send(err);
-    });
+        .then(result => {
+            console.log(`result`);
+            console.log(result[0].dataValues);
+            res.json(result[0].dataValues);
+            // res.redirect('/');
+        })
+        .catch(err => {
+            console.log(err)
+            res.send(err);
+        });
 })
 
-User.sync({force:false})
-.then(result=>console.log(result))
-.catch(err=>console.log(err));
+User.sync({ force: false })
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
 
 // sequelize.authenticate()
 // .then(con=>console.log(con))
 // .catch(err=>console.log(err));
-app.listen(3000,console.log(`listening on port 3000`));
+app.listen(3000, console.log(`listening on port 3000`));
--- public/index.js
+++ public/index.js
@@ -17,13 +17,42 @@ const save = (e) => {
     axios.post(url, obj)
         .then(data => {
             console.log(data);
-            if (data.data.name == 'SequelizeUniqueConstraintError')
+            if (data.data.name == 'SequelizeUniqueConstraintError') {
                 console.log('duplicate');
-            const showResult = document.getElementById('showResult');
-            showResult.innerHTML='User Already exist'
+                const showResult = document.getElementById('showResult');
+                showResult.innerHTML = 'User Already exist';
+            }
         })
         .catch(err => console.log(err));
     name.value = '';
+    email.value = '';
+    password.value = '';
+
+s}
+const login = (e) => {
+    // e.preventDefault();
+    console.log(e);
+    console.log('login');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const obj = {
+        email: email.value,
+        password: password.value
+    }
+    console.log(obj);
+
+    axios.post(`${url}login`, obj)
+        .then(data => {
+            console.log(`data`+data.data);
+            console.log(data.data.email);
+            if (data.data.email == obj.email) {
+                console.log('duplicate');
+                const showResult = document.getElementById('showResult');
+                showResult.innerHTML = 'User login successfully';
+                alert('user login successful')
+            }
+        })
+        .catch(err => console.log(err));
     email.value = '';
     password.value = '';
 
