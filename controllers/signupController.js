5642c2ef56492e71594ff0581884f9da987cb04e
S-123-satya <satyaprakash5056742@gmail.com>
2023-07-27 15:50:48 +0530

    token generated on signup also

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,11 +1,14 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../model/dbmodel');
+
+const secretKey = "secretKey";
 
 module.exports.postSignUpController = (req, res) => {
     console.log(`req.body.name=${req.body.name}`);
     console.log(`req.body.email=${req.body.email}`);
     console.log(`req.body.password=${req.body.password}`);
-    console.log(`req.body.User=${req.body}`);
+    console.log(req.body);
     bcrypt.hash(req.body.password, 10)
         .then(function (hash) {
             // Store hash in your password DB.
@@ -13,7 +16,19 @@ module.exports.postSignUpController = (req, res) => {
             User.create(req.body)
                 .then(result => {
                     console.log(result);
-                    res.json({message:"User login Succesfully",result:result});
+                    console.log(result.dataValues.id);
+                    const user = {
+                        userId: result.dataValues.id,
+                    }
+                    jwt.sign({ user }, secretKey, { expiresIn: '1000s' }, (err, token) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            res.json({ message: "User login Succesfully",token, name: result.dataValues.name });
+                        }
+                    })
+
                 })
                 .catch(err => {
                     console.log(err)
@@ -21,3 +36,21 @@ module.exports.postSignUpController = (req, res) => {
                 });
         });
 };
+
+//api for token generation
+// app.post('/jwt', (req, res) => {
+//     const user = {
+//         id: 1,
+//         name: 'bhavya',
+//         email: "bhavya@gmail.com",
+//         password: 'smart'
+//     }
+//     jwt.sign({ user }, secretKey, { expiresIn: '1000s' }, (err, token) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         else {
+//             res.json({ token })
+//         }
+//     })
+// })
